d77389947932e8f67caf1e9ececdecd7a36f0b15
austin-gathii <austin.gathii@strathmore.edu>
2022-01-15 11:28:54 +0300

    Editing some comments

--- facialRecognition.py
+++ facialRecognition.py
@@ -4,6 +4,8 @@ import numpy as np
 rootDir = "C://Users//pc//PycharmProjects//ICS_PROJECT//"
 mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40)
 #resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+#IMPORTING THE MAIN NEURAL NETWORK BEHIND THE FACIAL RECOGNITION
 resnet = pickle.load(open(rootDir+"resnetModel.p","rb"))
 
 def getFaceEmbedding(face):
